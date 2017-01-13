0f2a889345dc4642db4e15e0cc2f71962662c2fe
tosatake <toshiroh.satake@jrits.ricoh.co.jp>
2017-01-13 11:39:51 +0900

    Change shell script_2

--- Satake_practice.sh
+++ Satake_practice.sh
@@ -1,18 +1,1 @@
-#!/bin/sh -xe
-
-pwd
-whoami
-
-rbenv versions
-
-#Display text
-cat UBW.txt
-
-#Execute ruby
-ruby task_1.rb
-
-if [ $? = 0 ]; then
-  echo "Success!"
-else
-  echo "Failure..."
-fi
+#!/bin/sh -xepwdwhoamirbenv versions#Display textcat UBW.txt#Execute rubyruby task_1.rbif [ $? = 0 ]; then  echo "Success!"else  echo "Failure..."fi
